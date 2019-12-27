01b3d3c0814fc465c8abe793bd9ae7b9b8c7ad00
pankajladhar <ladharpankaj@gmail.com>
2019-12-27 23:56:53 +0530

    Add sign out in top nav bar

--- src/Components/TopBar/TopBar.js
+++ src/Components/TopBar/TopBar.js
@@ -1,18 +1,21 @@
 import React from "react";
 import CoditorLogo from "./../CoditorLogo/CoditorLogo";
+import { withRouter } from "react-router";
+import withFirebase from "./../../hooks/withFirebase";
 
-const TopBar = () => {
+const TopBar = ({ history, firebase }) => {
+  const onSignOut = () => {
+    firebase.auth().signOut();
+    history.push("/");
+  };
   return (
     <div className="Topbar bg-white px-8 py-2">
       <CoditorLogo size={80} name />
-      <a
-        className="underline cursor-pointer"
-        
-      >
+      <a className="underline cursor-pointer" onClick={onSignOut}>
         Sign-out
       </a>
     </div>
   );
 };
 
-export default TopBar;
+export default withFirebase(withRouter(TopBar));
