7e5ca3b4cbacd812e3151f782b4e66b5f4670775
lokesh-coder <mexican.dirtyfellow@gmail.com>
2019-12-29 21:41:05 +0530

    remove firebase config file

--- src/firebase.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  apiKey: "AIzaSyCr8_wrdnSp1W9ytEuAvNu9rxxpbBWvie4",
-  authDomain: "envy-app.firebaseapp.com",
-  databaseURL: "https://envy-app.firebaseio.com",
-  projectId: "envy-app",
-  storageBucket: "envy-app.appspot.com",
-  messagingSenderId: "499532076935",
-  appId: "1:499532076935:web:bb3fea7556e53481aa6522"
-};
